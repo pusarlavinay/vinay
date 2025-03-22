84eb30485577be99d61c61d527c287d8961b17ed
Pusarla Vinay <vinaypusarla22@gmail.com>
2025-03-22 14:12:24 +0530

    Add files via upload

--- set_webhook.py
+++ set_webhook.py
@@ -1,8 +1,8 @@
 import requests
 import os
 
-TOKEN = os.getenv("TELEGRAM_TOKEN")  # Get from environment variables
-WEBHOOK_URL = os.getenv("RENDER_URL") + "/my_secret_path"  # Append webhook path
+TOKEN = os.getenv("7349721276:AAE6ZPaQ5gr2pfTzwD4fHdvE3oEayypxtuk")  # Get from environment variables
+WEBHOOK_URL = os.getenv("https://vinay-zkni.onrender.com") + "/my_secret_path"  # Append webhook path
 
 response = requests.get(f"https://api.telegram.org/bot{TOKEN}/setWebhook?url={WEBHOOK_URL}")
 print(response.json())
